5672fa4028ad2f70a4bd6a517223ff099264a8ec
Anderson Correia de Souza <anderson_souza33@outlook.com.br>
2021-01-26 11:58:52 -0300

    feat: feito o resource de produto

---
+++ src/main/java/com/anderson/estoque/resource/ProdutoResource.java
@@ -0,0 +1,61 @@
+package com.anderson.estoque.resource;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+public class ProdutoResource {
+
+    @Id
+    private String id;
+    @Column(length = 35, nullable = false)
+    private String nome;
+    @Column(length = 30, nullable = false)
+    private String marca;
+    @Column(precision = 6, scale = 2, nullable = false)
+    private BigDecimal preco;
+    @Column(columnDefinition = "int(3)", nullable = false)
+    private int quantidade;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+}
