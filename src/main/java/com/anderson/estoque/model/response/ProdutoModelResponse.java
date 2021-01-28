0bd792ced9d08db9bc7fb0dc7f4c644595d46af4
Anderson Correia de Souza <anderson_souza33@outlook.com.br>
2021-01-28 11:46:11 -0300

    feat: criado a classe ProdutoModelResponse

---
+++ src/main/java/com/anderson/estoque/model/response/ProdutoModelResponse.java
@@ -0,0 +1,52 @@
+package com.anderson.estoque.model.response;
+
+import java.math.BigDecimal;
+
+public class ProdutoModelResponse {
+
+    private String id;
+    private String nome;
+    private String marca;
+    private BigDecimal preco;
+    private int quantidade;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+}
