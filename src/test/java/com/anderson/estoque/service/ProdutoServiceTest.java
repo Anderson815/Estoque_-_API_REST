c60b1b10c472daa763b8145de71ba371e95cddd9
Anderson Correia de Souza <anderson_souza33@outlook.com.br>
2021-02-12 19:21:01 -0300

    test(ProdutoServiceTest): testes do método obterProdutosPelaMarca(...)

--- src/test/java/com/anderson/estoque/service/ProdutoServiceTest.java
+++ src/test/java/com/anderson/estoque/service/ProdutoServiceTest.java
@@ -1,10 +1,13 @@
 package com.anderson.estoque.service;
 
+import com.anderson.estoque.exception.NotFoundException;
 import com.anderson.estoque.model.request.ProdutoModelRequest;
 import com.anderson.estoque.model.response.ProdutoModelResponse;
 import com.anderson.estoque.repository.ProdutoRepository;
 import com.anderson.estoque.resource.ProdutoResource;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import static org.junit.Assert.*;
 import org.mockito.InjectMocks;
@@ -25,6 +28,10 @@ public class ProdutoServiceTest {
     @InjectMocks
     private ProdutoService produtoService;
 
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    //Teste do método criar()
     @Test
     public void testCriarComSucesso(){
 
@@ -39,6 +46,7 @@ public class ProdutoServiceTest {
         assertNotEquals("", produtoResponse.getId());
     }
 
+    //Teste do método obterProdutos()
     @Test
     public void testObterProdutosComSucesso(){
 
@@ -105,5 +113,89 @@ public class ProdutoServiceTest {
         //Teste
         List<ProdutoModelResponse> listaProdutoResponseAtual = produtoService.obterProdutos();
         assertArrayEquals(listaProdutoResponseEsperado.toArray(), listaProdutoResponseAtual.toArray());
+    }
+
+    //Testes do método obterProdutosPelaMarca(...)
+    @Test
+    public void testObterProdutosPelaMarcaComSucesso(){
+
+        //Parâmetro
+        String marca = "Sony";
+
+        //Preparo
+        ProdutoResource produtoResource1 = new ProdutoResource();
+        produtoResource1.setId("abcde");
+        produtoResource1.setNome("Smart TV");
+        produtoResource1.setMarca("Sony");
+        produtoResource1.setPreco(new BigDecimal("3500.69"));
+        produtoResource1.setQuantidade(89);
+
+        ProdutoResource produtoResource3 = new ProdutoResource();
+        produtoResource3.setId("klmno");
+        produtoResource3.setNome("PlayStation 5");
+        produtoResource3.setMarca("Sony");
+        produtoResource3.setPreco(new BigDecimal("4685.27"));
+        produtoResource3.setQuantidade(30);
+
+        //Objeto para simulação
+        List<ProdutoResource> listaProdutoResource = new ArrayList<>();
+        listaProdutoResource.add(produtoResource1);
+        listaProdutoResource.add(produtoResource3);
+
+        //Simulação
+        when(produtoRepository.existsByMarca(marca))
+                .thenReturn(true);
+
+        when(produtoRepository.findAllByMarca(marca))
+                .thenReturn(listaProdutoResource);
+
+        //Preparo do esperado
+        ProdutoModelResponse produtoResponse1 = new ProdutoModelResponse();
+        produtoResponse1.setId("abcde");
+        produtoResponse1.setNome("Smart TV");
+        produtoResponse1.setMarca("Sony");
+        produtoResponse1.setPreco(new BigDecimal("1550.75"));
+        produtoResponse1.setQuantidade(89);
+
+        ProdutoModelResponse produtoResponse2 = new ProdutoModelResponse();
+        produtoResponse2.setId("fghij");
+        produtoResponse2.setNome("iPhone 12");
+        produtoResponse2.setMarca("Apple");
+        produtoResponse2.setPreco(new BigDecimal("7899.99"));
+        produtoResponse2.setQuantidade(150);
+
+        ProdutoModelResponse produtoResponse3 = new ProdutoModelResponse();
+        produtoResponse3.setId("klmno");
+        produtoResponse3.setNome("PlayStation 5");
+        produtoResponse3.setMarca("Sony");
+        produtoResponse3.setPreco(new BigDecimal("4685.27"));
+        produtoResponse3.setQuantidade(30);
+
+        //Esperado
+        List<ProdutoModelResponse> listaProdutoResponseEsperado = new ArrayList<>();
+        listaProdutoResponseEsperado.add(produtoResponse1);
+        listaProdutoResponseEsperado.add(produtoResponse3);
+
+        //Teste
+        List<ProdutoModelResponse> listaProdutoResponseAtual = produtoService.obterProdutosPelaMarca(marca);
+        assertArrayEquals(listaProdutoResponseEsperado.toArray(), listaProdutoResponseAtual.toArray());
+    }
+
+    @Test
+    public void testObterProdutosPelaMarcaFalhaMarcaInexistente(){
+
+        //Parâmetro
+        String marca = "Sony";
+
+        //Esperado
+        thrown.expect(NotFoundException.class);
+        thrown.expectMessage("Não foi possível encontrar a marca");
+
+        //Simulação
+        when(produtoRepository.existsByMarca(marca))
+                .thenReturn(false);
+
+        //Teste
+        List<ProdutoModelResponse> listaProdutoResponseAtual = produtoService.obterProdutosPelaMarca(marca);
     }
 }
